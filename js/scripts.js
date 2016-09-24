53b309071a79fd62b284066e75af856d4715a607
Alex Brown <alex.brown@SecData.com>
2016-09-24 16:40:01 +0100

    Updated Avatar, Contact links replaced with coder relevant one. Removed Facbooke and twitter shizzle

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
+
 var callback = function(){
+	// Rezise the skillz graphs
 	$('.item-skills').each(function(){
 		newWidth = $(this).parent().width() * $(this).data('percent');
 		$(this).width(0);
@@ -6,19 +8,28 @@ var callback = function(){
         width: newWidth,
     }, 1000);
 	});
+	// 
 	$('.icons-red').each(function(){
 		height = $(this).height();
     $(this).animate({
         height: 14,
     }, 2000);
 	});
+
 };
+
+// Standard JQuery on document ready  
 $(document).ready(callback);
 
 var resize;
+
+// Call 100ms after window resize
 window.onresize = function() {
+
 	clearTimeout(resize);
+
 	resize = setTimeout(function(){
 		callback();
 	}, 100);
+
 };
